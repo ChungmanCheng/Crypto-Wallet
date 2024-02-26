2967254acc6fdaf65f65eead8bde66527cc134a3
erwin <erwinchengchungman916@gmail.com>
2024-02-26 10:26:32 +0800

    Added import & create account feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,215 @@
-import Image from "next/image";
+
+'use client';
+
+import { useState } from 'react';
+import { ethers } from 'ethers';
+
+interface WalletInfo {
+    privateKey: string;
+	mnemonic: string;
+    address: string;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [isCreateAccount, setIsCreateAccount] = useState<boolean>(false);
+	const [isImportAccount, setIsImportAccount] = useState<boolean>(false);
+	const [isConfirmAccount, setIsConfirmAccount] = useState<boolean>(false);
+	const [walletInfo, setWalletInfo] = useState<WalletInfo | null>(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [recoveryPhrase, setRecoveryPhrase] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleNext = () => {
+		setCurrentPage(currentPage + 1); // Increment current page
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const handleComplete = () => {
+		if (recoveryPhrase === walletInfo?.mnemonic) {
+			setIsConfirmAccount(true);
+			setIsCreateAccount(false);
+		  	setErrorMessage('');
+		} else {
+		  	setErrorMessage('Recovery phrase does not match.');
+		}
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+	const createWallet = async () => {
+		setIsCreateAccount(true);
+		setCurrentPage(1);
+		const randomWallet = ethers.Wallet.createRandom();
+		const walletData = {
+			privateKey: randomWallet.privateKey,
+			mnemonic: randomWallet.mnemonic.phrase,
+			address: randomWallet.address,
+		};
+		setWalletInfo(walletData);
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	const handleImport = () => {
+		try {
+			const wallet = ethers.Wallet.fromPhrase(recoveryPhrase);
+			setWalletInfo(wallet);
+			setErrorMessage('');
+			setIsImportAccount(false);
+			setIsConfirmAccount(true);
+		  } catch (error) {
+			// If an error occurs during wallet creation, handle it appropriately
+			console.log(error);
+			setErrorMessage('Error importing recovery phrase. Please check and try again.');
+		  }
+	}
+
+	const importWallet = async () => {
+		setIsImportAccount(true);
+	};
+
+	return (
+		<main className="flex min-h-screen p-24">
+			<div className="z-10 w-full font-mono text-sm lg:flex">
+				<div>
+					<div className="m-5">
+						<div className="flex flex-col col-span-full md:col-span-6 xl:col-span-4 bg-white dark:bg-[#202123] shadow-lg rounded-3xl border border-slate-200 dark:border-slate-700">
+							<div className="px-5 py-5">
+								<h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">Wallet</h2>
+								<div className="text-xs font-semibold text-slate-400 dark:text-slate-500 uppercase mb-1">ETH</div>
+								{(isConfirmAccount && walletInfo)?(
+									<div>
+										<div className="flex items-start">
+											<div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">0</div>
+										</div>
+										<div className="text-sm font-medium text-slate-500 dark:text-slate-400 mt-2">
+											<button onClick={() => {}} className="mr-2 hover:text-[#8ad1c2]">Deposit</button>
+											<button onClick={() => {}} className="mx-2 hover:text-[#8ad1c2]">Withdraw</button>
+											<button onClick={() => {}} className="ml-2 hover:text-[#8ad1c2]">Send</button>
+										</div>
+									</div>
+								):(
+									<div>
+										<button onClick={createWallet} className="mr-2 hover:text-[#8ad1c2]">Create Wallet</button>
+										<p>or</p>
+										<button onClick={importWallet} className="mr-2 hover:text-[#8ad1c2]">Import Wallet</button>
+									</div>
+								)}
+								{isCreateAccount?(
+								<div className="justify-center items-end sm:items-center m-3 sm:m-0 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+									<div className="relative sm:my-8 w-full sm:max-w-2xl">
+										<div className="border-0 rounded-xl shadow-lg relative flex flex-col w-full bg-gray-50 dark:bg-[#202123] outline-none focus:outline-none bg-clip-padding h-[632px] sm:h-[472px]">
+											{currentPage === 1 && (
+											<>
+												<div className="flex flex-col items-center justify-center px-5 border-solid border-gray-300 dark:border-gray-700 rounded-t h-1/6">
+													<h3 className="text-2xl font-semibold dark:text-gray-200">
+														Secret Recovery Phrase
+													</h3>
+												</div>
+												<div className="relative text-gray-500 border-gray-200 dark:border-gray-700 dark:text-gray-400 h-3/5 flex flex-col justify-center mx-10 ml-20">                  
+													<p className="mb-4 text-sm text-gray-600 dark:text-gray-300">
+														Please remember that this is your secret recovery phrase. Keep it secure and do not disclose it to anyone.
+													</p>
+													{walletInfo?.mnemonic}
+												</div>
+												<div className="flex items-center justify-center px-6 border-solid border-gray-300 dark:border-gray-700 rounded-b gap-2.5 h-1/5">
+													<button
+													className="w-36 bg-[#8ad1c2] text-white active:bg-teal-500 font-bold uppercase py-3 text-sm rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+													onClick={() => handleNext()}
+													>
+													Next
+													</button>
+													<button
+													className="w-36 bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-sm py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+													onClick={() => setIsCreateAccount(false)}
+													>
+													Cancel
+													</button>
+												</div>
+											</>
+											)}
+											{currentPage === 2 && (
+											<>
+												<div className="flex flex-col items-center justify-center px-5 border-solid border-gray-300 dark:border-gray-700 rounded-t h-1/6">
+													<h3 className="text-2xl font-semibold dark:text-gray-200">
+														Confirm your Secret Recovery Phrase
+													</h3>
+												</div>
+												<div className="relative text-gray-500 border-gray-200 dark:border-gray-700 dark:text-gray-400 h-3/5 flex flex-col justify-center mx-10 ml-20">                  
+													<input
+														type="text"
+														className="border border-gray-300 dark:border-gray-700 rounded-md px-3 py-2 mb-3 focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-800 dark:text-gray-200"
+														placeholder="Enter your recovery phrase"
+														value={recoveryPhrase}
+														onChange={(e) => setRecoveryPhrase(e.target.value)}
+													/>
+													{errorMessage && <p className="text-red-500">{errorMessage}</p>}
+												</div>
+												<div className="flex items-center justify-center px-6 border-solid border-gray-300 dark:border-gray-700 rounded-b gap-2.5 h-1/5">
+													<button
+													className="w-36 bg-[#8ad1c2] text-white active:bg-teal-500 font-bold uppercase py-3 text-sm rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+													onClick={() => handleComplete()}
+													>
+													Complete
+													</button>
+													<button
+													className="w-36 bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-sm py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+													onClick={() => setIsCreateAccount(false)}
+													>
+													Cancel
+													</button>
+												</div>
+											</>
+											)}
+										</div>
+									</div>
+								</div>
+								):(
+								<>
+								</>
+								)}
+
+								{isImportAccount?(
+								<div className="justify-center items-end sm:items-center m-3 sm:m-0 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+									<div className="relative sm:my-8 w-full sm:max-w-2xl">
+										<div className="border-0 rounded-xl shadow-lg relative flex flex-col w-full bg-gray-50 dark:bg-[#202123] outline-none focus:outline-none bg-clip-padding h-[632px] sm:h-[472px]">
+											<div className="flex flex-col items-center justify-center px-5 border-solid border-gray-300 dark:border-gray-700 rounded-t h-1/6">
+												<h3 className="text-2xl font-semibold dark:text-gray-200">
+													Import your Secret Recovery Phrase
+												</h3>
+											</div>
+											<div className="relative text-gray-500 border-gray-200 dark:border-gray-700 dark:text-gray-400 h-3/5 flex flex-col justify-center mx-10 ml-20">                  
+												<input
+													type="text"
+													className="border border-gray-300 dark:border-gray-700 rounded-md px-3 py-2 mb-3 focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-800 dark:text-gray-200"
+													placeholder="Enter your recovery phrase"
+													value={recoveryPhrase}
+													onChange={(e) => setRecoveryPhrase(e.target.value)}
+												/>
+												{errorMessage && <p className="text-red-500">{errorMessage}</p>}
+											</div>
+											<div className="flex items-center justify-center px-6 border-solid border-gray-300 dark:border-gray-700 rounded-b gap-2.5 h-1/5">
+												<button
+												className="w-36 bg-[#8ad1c2] text-white active:bg-teal-500 font-bold uppercase py-3 text-sm rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+												onClick={() => handleImport()}
+												>
+												Complete
+												</button>
+												<button
+												className="w-36 bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-sm py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+												onClick={() => setIsImportAccount(false)}
+												>
+												Cancel
+												</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								):(
+									<>
+									</>
+								)}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
